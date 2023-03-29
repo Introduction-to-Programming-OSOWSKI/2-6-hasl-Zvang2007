811bb575225b2cd214b5325e9b9478fdb0014bff
Zvang2007 <833148@mystma.org>
2023-03-29 15:14:52 -0500

    1

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-#WRITE YOUR CODE IN THIS FILE
+def hasL(w):
+   for i in range (0,len(w)):
+        numL = 0
+        if w [i] == "alabama":
+            numL = numL + 1
+            return True
+            
+    return False
+
+
