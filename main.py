5b8d26f7aa104d4b0ee048b66f754d9fc1425286
Zvang2007 <833148@mystma.org>
2023-03-30 14:56:15 -0500

    5

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 def hasL(w):
    for i in range (0,len(w)):
-        numL = 0
-        if w [i] == "l":
+     numL = 0
+     if w [i] == "l":
+            numL = numL + 1
             return True
-    return False
+        return False
 
-
+hasL()
