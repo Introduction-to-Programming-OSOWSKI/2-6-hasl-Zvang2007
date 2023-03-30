78a414b5f33eeb683fccd7a55aed45e66737797f
Zvang2007 <833148@mystma.org>
2023-03-30 14:47:14 -0500

    2

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
 def hasL(w):
    for i in range (0,len(w)):
         numL = 0
-        if w [i] == "alabama":
-            numL = numL + 1
+        if w [i] == "l":
             return True
-            
     return False
 
 
